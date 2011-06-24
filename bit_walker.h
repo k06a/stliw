afe14e362bace377f5cb2e8a2b953d62b3677b13
k06a <k06a@localhost>
2011-06-25 02:03:31 +0400

    first commit

---
+++ bit_walker.h
@@ -0,0 +1,214 @@
+#include <iterator>
+
+/// --------------------------------------------------------------------
+
+template<typename Category,
+         typename IterType,
+         typename T = int>
+class bit_walk_iterator
+    : public std::iterator<Category,T>
+{
+public:
+    IterType it;
+    T nextBit;
+
+public:
+    bit_walk_iterator()
+        : it(), nextBit(T())
+    {
+    }
+
+    bit_walk_iterator(IterType & it)
+        : it(it), nextBit(T())
+    {
+    }
+
+    // Base methods
+
+    bit_walk_iterator & operator ++ ()
+    {
+        nextBit++;
+        if (nextBit == CHAR_BIT*sizeof(value_type))
+        {
+            ++it;
+            nextBit = 0;
+        }
+        return *this;
+    }
+
+    bit_walk_iterator operator ++ (int) 
+    {
+        bit_walk_iterator tmp(*this);
+        operator++();
+        return tmp;
+    }
+
+    // Input category methods
+
+    bool operator == (const bit_walk_iterator & rhs) const
+    {
+        return (it == rhs.it) && (nextBit == rhs.nextBit);
+    }
+
+    bool operator != (const bit_walk_iterator & rhs) const
+    {
+        return !(*this == rhs);
+    }
+
+    const T operator * () const
+    {
+        int byteNumber = nextBit / 8;
+        int bitNumber  = nextBit % 8;
+        char & ch = byteNumber[(char*)&(*it)];
+        if ((*it) & (1 << bitNumber))
+            return 1;
+        return 0;
+    }
+
+    // Output category methods
+
+    bit_walk_iterator & operator * ()
+    {
+        return *this;
+    }
+
+    bit_walk_iterator & operator = (int x)
+    {
+        int byteNumber = nextBit / 8;
+        int bitNumber  = nextBit % 8;
+        char & ch = byteNumber[(char*)&(*it)];
+        ch &= (-1)^(1 << bitNumber);
+        ch |= ((x?1:0) << bitNumber)
+        return *this;
+    }
+
+    // Bidirectional category methods
+
+    bit_walk_iterator & operator -- ()
+    {
+        nextBit--;
+        if (nextBit == -1)
+        {
+            --it;
+            nextBit = CHAR_BIT*sizeof(value_type)-1;
+        }
+        return *this;
+    }
+
+    bit_walk_iterator operator -- (int) 
+    {
+        bit_walk_iterator tmp(*this);
+        operator--();
+        return tmp;
+    }
+
+    // Random access category methods
+
+private:
+    bit_walk_iterator & move()
+    {
+        int bitsInObject = CHAR_BIT*sizeof(value_type);
+
+        if (nextBit < 0)
+        {
+            it -= ((-nextBit) / bitsInObject)
+                  + ((-nextBit) % bitsInObject > 0);
+            nextBits = bitsInObject - ((-nextBit) % bitsInObject);
+        } else
+        if (nextBit > bitsInObject)
+        {
+            it += nextBit / bitsInObject;
+            nextBit %= bitsInObject;
+        }
+        
+        return *this;
+    }
+
+public:
+    bit_walk_iterator & operator += (int n)
+    {
+        nextBit += n;
+        return move();
+    }
+
+    bit_walk_iterator & operator -= (int n)
+    {
+        nextBit -= n;
+        return move();
+    }
+
+    bool operator < (const bit_walk_iterator & der) const
+    {
+        return (it < der.it);
+    }
+
+    bool operator > (const bit_walk_iterator & der) const
+    {
+        return (it > der.it);
+    }
+
+    bool operator <= (const bit_walk_iterator & der) const
+    {
+        return (it <= der.it);
+    }
+
+    bool operator >= (const bit_walk_iterator & der) const
+    {
+        return (it >= der.it);
+    }
+
+    bit_walk_iterator operator [] (int n) const
+    {
+        bit_walk_iterator tmp(*this);
+        return tmp += n;
+    }
+};
+
+template<typename Category, typename IterType, typename T>
+bit_walk_iterator<Category,IterType,T> operator + (
+    const bit_walk_iterator<Category,IterType,T> & der, int n)
+{
+    bit_walk_iterator<Category,IterType,T> tmp(der);
+    return tmp += n;
+}
+
+template<typename Category, typename IterType, typename T>
+bit_walk_iterator<Category,IterType,T> operator + (
+    int n, const bit_walk_iterator<Category,IterType,T> & der)
+{
+    bit_walk_iterator<Category,IterType,T> tmp(der);
+    return tmp += n;
+}
+
+template<typename Category, typename IterType, typename T>
+bit_walk_iterator<Category,IterType,T> operator - (
+    const bit_walk_iterator<Category,IterType,T> & der, int n)
+{
+    bit_walk_iterator<Category,IterType,T> tmp(der);
+    return tmp -= n;
+}
+
+template<typename Category, typename IterType, typename T>
+ptrdiff_t operator - (
+    const bit_walk_iterator<Category,IterType,T> & a,
+    const bit_walk_iterator<Category,IterType,T> & b)
+{
+    return (a.it - b.it) + (a.nextBit - b.nextBit);
+}
+
+/// --------------------------------------------------------------------
+
+template<typename T, typename IterType>
+bit_walk_iterator<typename std::iterator_traits<IterType>::iterator_category,IterType,T>
+bit_walker(IterType it)
+{
+    return it;
+}
+/*
+template<typename IterType>
+bit_walk_iterator<typename std::iterator_traits<IterType>::iterator_category,IterType,int>
+bit_walker(IterType it)
+{
+    return it;
+}
+*/
---
+++ field_walker.h
@@ -0,0 +1,162 @@
+#include <iterator>
+
+#define fieldof(Object,field) (&(((Object*)NULL)->field))
+
+/// --------------------------------------------------------------------
+
+template<typename Category,
+         typename IterType,
+         typename FieldType>
+class field_walk_iterator
+    : public std::iterator<Category,FieldType>
+{
+public:
+    IterType it;
+    FieldType * field;
+
+public:
+    field_walk_iterator(FieldType * field)
+        : it(IterType()), field(field)
+    {
+    }
+
+    field_walk_iterator(IterType & it, FieldType * field)
+        : it(it), field(field)
+    {
+    }
+
+    // Base methods
+
+    field_walk_iterator & operator ++ ()
+    {
+        ++it;
+        return *this;
+    }
+
+    field_walk_iterator operator ++ (int) 
+    {
+        field_walk_iterator tmp(*this);
+        operator++();
+        return tmp;
+    }
+
+    // Input category methods
+
+    bool operator == (const field_walk_iterator & rhs) const
+    {
+        return (it == rhs.it);
+    }
+
+    bool operator != (const field_walk_iterator & rhs) const
+    {
+        return !(*this == rhs);
+    }
+
+    FieldType operator * () const
+    {
+        return *(FieldType&*)((long)field) + ((long)&*it)
+    }
+
+    FieldType & operator * ()
+    {
+        long a = (long)&(*it);
+        long b = (long)field;
+        return *(FieldType*)(a + b);
+    }
+
+    // Bidirectional category methods
+
+    field_walk_iterator & operator -- ()
+    {
+        --it;
+        return *this;
+    }
+
+    field_walk_iterator operator -- (int) 
+    {
+        field_walk_iterator tmp(*this);
+        operator--();
+        return tmp;
+    }
+
+    // Random access category methods
+
+    field_walk_iterator & operator += (int n)
+    {
+        it += n;
+        return *this;
+    }
+
+    field_walk_iterator & operator -= (int n)
+    {
+        it -= n;
+        return *this;
+    }
+
+    bool operator < (const field_walk_iterator & der) const
+    {
+        return (it < der.it);
+    }
+
+    bool operator > (const field_walk_iterator & der) const
+    {
+        return (it > der.it);
+    }
+
+    bool operator <= (const field_walk_iterator & der) const
+    {
+        return (it <= der.it);
+    }
+
+    bool operator >= (const field_walk_iterator & der) const
+    {
+        return (it >= der.it);
+    }
+
+    field_walk_iterator operator [] (int n) const
+    {
+        field_walk_iterator tmp(*this);
+        return tmp += n;
+    }
+};
+
+template<typename Category, typename IterType, typename T>
+field_walk_iterator<Category,IterType,T> operator + (
+    const field_walk_iterator<Category,IterType,T> & der, int n)
+{
+    field_walk_iterator<Category,IterType,T> tmp(der);
+    return tmp += n;
+}
+
+template<typename Category, typename IterType, typename T>
+field_walk_iterator<Category,IterType,T> operator + (
+    int n, const field_walk_iterator<Category,IterType,T> & der)
+{
+    field_walk_iterator<Category,IterType,T> tmp(der);
+    return tmp += n;
+}
+
+template<typename Category, typename IterType, typename T>
+field_walk_iterator<Category,IterType,T> operator - (
+    const field_walk_iterator<Category,IterType,T> & der, int n)
+{
+    field_walk_iterator<Category,IterType,T> tmp(der);
+    return tmp -= n;
+}
+
+template<typename Category, typename IterType, typename T>
+ptrdiff_t operator - (
+    const field_walk_iterator<Category,IterType,T> & a,
+    const field_walk_iterator<Category,IterType,T> & b)
+{
+    return (a.it - b.it);
+}
+
+/// --------------------------------------------------------------------
+
+template<typename IterType, typename T>
+field_walk_iterator<typename std::iterator_traits<IterType>::iterator_category,IterType,T>
+field_walker(IterType it, T * field)
+{
+    return field_walk_iterator<typename std::iterator_traits<IterType>::iterator_category,IterType,T>(it,field);
+}
